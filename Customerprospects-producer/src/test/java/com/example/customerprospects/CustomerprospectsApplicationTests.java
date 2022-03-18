3c2c1fb9df7f449bb63e630dbbaa306bf9110e01
Vinod Ramasubbu <79949043+vinodramasubbu@users.noreply.github.com>
2022-03-18 13:46:34 -0400

    Add files via upload

---
+++ Customerprospects-producer/src/test/java/com/example/customerprospects/CustomerprospectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customerprospects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerprospectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
